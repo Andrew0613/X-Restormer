b4d5e36bf5d91dded5c6ca16e3a279091837c86b
Andrew0613 <puyuandong01061313@gmail.com>
2023-08-26 10:18:47 +0000

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 import time
 
-version_file = 'basicsrexamples/version.py'
+version_file = 'xrestormer/version.py'
 
 
 def readme():
@@ -82,15 +82,15 @@ def get_requirements(filename='requirements.txt'):
 if __name__ == '__main__':
     write_version_py()
     setup(
-        name='basicsrexamples',
+        name='xrestormer',
         version=get_version(),
-        description='BasicSR Examples',
+        description='xrestormer',
         long_description=readme(),
         long_description_content_type='text/markdown',
-        author='Xintao Wang',
-        author_email='xintao.wang@outlook.com',
+        author='Yuandong Pu',
+        author_email='puyuandong@pjlab.org.cn',
         keywords='computer vision, pytorch, basicsr, image restoration, super-resolution',
-        url='https://github.com/xinntao/BasicSR-examples',
+        url='https://github.com/Andrew0613/X-Restormer',
         include_package_data=True,
         packages=find_packages(exclude=('options', 'datasets', 'experiments', 'results', 'tb_logger', 'wandb')),
         classifiers=[
---
+++ xrestormer/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Sat Aug 26 10:05:25 2023
+__version__ = '0.1.0'
+__gitsha__ = '767f843'
+version_info = (0, 1, 0)
